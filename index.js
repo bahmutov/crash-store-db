99e13d44416e771b8b4068cceb63d52ba026a22e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-15 18:07:56 -0500

    some primitive interface

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var la = require('lazy-ass');
+var check = require('check-more-types');
+var Promise = require('bluebird');
+
 function testDummy() {
   console.log('testing dummy app key');
   var AppKey = require('./src/models/app-keys');
@@ -9,10 +13,28 @@ function testDummy() {
   });
 }
 
+function addApplicationKey(db, name) {
+  la(db, 'missing db');
+  la(check.unemptyString(name), 'missing app name', name);
+  return Promise.reject();
+}
+
+function isValidApplicationKey(db, key) {
+  return Promise.reject();
+}
+
+function storeException(db, key, exceptionInformation) {
+  return Promise.reject();
+}
+
 function initCrashStoreDatabase() {
   var dbInit = require('./src/db');
   return dbInit.then(function (db) {
     console.log('initialized db store');
+
+    db.addApplicationKey = addApplicationKey.bind(null, db);
+    db.isValidApplicationKey = isValidApplicationKey.bind(null, db);
+    db.storeException = storeException.bind(null, db);
     return db;
   });
 }
