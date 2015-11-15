9cde7350e1b4682dec79d16956076f2e00a58fd3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-15 18:03:49 -0500

    working as stand alone

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-var config = require('./src/config');
-var getConfigured = config.get.bind(config);
-
-var http = require('http');
-var app = require('express')();
-var initCrashReporter = require('crash-reporter-middleware');
-
-var la = require('lazy-ass');
-var check = require('check-more-types');
-
-var AppKey = require('./src/models/app-keys');
-
-initCrashReporter(getConfigured, app)
-  .then(function (crashMiddleware) {
-    if (crashMiddleware) {
-      console.log('using crash reporter middleware');
-      app.use(crashMiddleware);
-    }
-
-    var dbInit = require('./src/db');
-    dbInit.then(function (db) {
-      var port = getConfigured('PORT');
-      http.createServer(app).listen(port);
-      console.log('listening at port %d', port);
-
-      AppKey.saveDummy().then(function (key) {
-        console.log('created dummy application key');
-        console.log(key);
-      }, function (err) {
-        console.error(err);
-      });
-
-    }, function (err) {
-      console.error(err);
-      process.exit(-1);
-    });
+function testDummy() {
+  console.log('testing dummy app key');
+  var AppKey = require('./src/models/app-keys');
+  return AppKey.saveDummy().then(function (key) {
+    console.log('created dummy application key');
+    console.log(key);
+  }, function (err) {
+    console.error(err);
   });
+}
+
+function initCrashStoreDatabase() {
+  var dbInit = require('./src/db');
+  return dbInit.then(function (db) {
+    console.log('initialized db store');
+    return db;
+  });
+}
+
+module.exports = initCrashStoreDatabase;
+
+if (!module.parent) {
+  console.log('running stand alone');
+  initCrashStoreDatabase()
+    .then(testDummy)
+    .catch(console.error.bind(console));
+}
